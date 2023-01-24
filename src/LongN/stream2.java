7cc2e3109e40f69242bf5de9c3fa6c18b6ec428f
Long-N <longnguyen.avt@gmail.com>
2023-01-23 20:13:56 -0600

    stream2 created

---
+++ src/LongN/stream2.java
@@ -0,0 +1,11 @@
+package LongN;
+
+import java.util.Arrays;
+
+public class stream2 {
+    public static void main(String[] args) {
+        String[] str = new String[]{"aksjdhaksjdh"};
+     
+
+    }
+}
